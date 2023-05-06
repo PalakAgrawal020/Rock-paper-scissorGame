daf4a3fc461fe3be74f97e55f0302cb7460fd533
Palak <palakagrawal020@gmail.com>
2023-05-06 19:55:38 +0530

    Another function which compares the choices and declares result

--- game.js
+++ game.js
@@ -4,4 +4,36 @@ function getComputerChoice() {
     return result;
 }
 
-console.log(getComputerChoice());
+function round(playerSelection, computerSelection) {
+    if(playerSelection == computerSelection){
+        return "It's a tie";
+    }
+    if(playerSelection == 'ROCK'){
+        if(computerSelection == 'PAPER'){
+            return "You lose! Paper beats Rock";
+        }
+        else if(computerSelection == 'SCISSOR'){
+            return "You win! Rock beats Scissor";
+        }
+    }
+    if(playerSelection == 'PAPER'){
+        if(computerSelection == 'ROCK'){
+            return "You win! Paper beats Rock";
+        }
+        else if(computerSelection == 'SCISSOR'){
+            return "You lose! Scissor beats Paper";
+        }
+    }
+    if(playerSelection == 'SCISSOR'){
+        if(computerSelection == 'ROCK'){
+            return "You lose! Rock beats Scissor";
+        }
+        else if(computerSelection == 'PAPER'){
+            return "You win! Scissor beats Paper";
+        }
+    }
+}
+
+let playerSelection = 'ROck';
+let computerSelection = getComputerChoice();
+console.log(round(playerSelection.toUpperCase(), computerSelection.toUpperCase()));
